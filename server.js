386b391c569c31f483502c8a39a39b62395663b0
fongelias <fongelias@gmail.com>
2018-07-20 21:48:48 -0400

    added mongoose connection to mongodb

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
+//setup for mongoDB
+mongoose.connect("mongodb://mongo:27017");
+
+//routes
 app.get('/', (req, res) => {
 	res.send("Hello World");
 });
 
+//listen to port and print message
 const server = app.listen(3000, () => {
 	console.log('hello world server listening on port 3000');
 });
